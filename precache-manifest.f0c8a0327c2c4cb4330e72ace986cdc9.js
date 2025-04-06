d2e98a8c1ee2234630d53e7f3df4c84cf213ff93
Gonzalo Vallejos <gonza18av@gmail.com>
2025-04-06 19:11:30 -0300

    Updates

---
+++ precache-manifest.f0c8a0327c2c4cb4330e72ace986cdc9.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1424bbc930649627f853ea543c21341f",
+    "url": "/mi-portafolio/index.html"
+  },
+  {
+    "revision": "7bbbf4dfb2e08c798036",
+    "url": "/mi-portafolio/static/css/2.2bf599ca.chunk.css"
+  },
+  {
+    "revision": "96846326f4cc7a4a7b48",
+    "url": "/mi-portafolio/static/css/main.d016105a.chunk.css"
+  },
+  {
+    "revision": "7bbbf4dfb2e08c798036",
+    "url": "/mi-portafolio/static/js/2.48601e4b.chunk.js"
+  },
+  {
+    "revision": "70c068b2fb9d959ad245",
+    "url": "/mi-portafolio/static/js/3.730e5367.chunk.js"
+  },
+  {
+    "revision": "96846326f4cc7a4a7b48",
+    "url": "/mi-portafolio/static/js/main.daa14194.chunk.js"
+  },
+  {
+    "revision": "a2b26de71944bd2d8ec9",
+    "url": "/mi-portafolio/static/js/runtime~main.6022f20f.js"
+  },
+  {
+    "revision": "3a0cb82447f8e9ce865a709a92f0c752",
+    "url": "/mi-portafolio/static/media/boxicons.3a0cb824.woff"
+  },
+  {
+    "revision": "4002d1c83d8dd40df85708c5601993e4",
+    "url": "/mi-portafolio/static/media/boxicons.4002d1c8.eot"
+  },
+  {
+    "revision": "878061312a371566f591f1f1a9f76a87",
+    "url": "/mi-portafolio/static/media/boxicons.87806131.ttf"
+  },
+  {
+    "revision": "aab73283f839e775f9ac86d642983653",
+    "url": "/mi-portafolio/static/media/boxicons.aab73283.woff2"
+  },
+  {
+    "revision": "b0bb967778275b356010f01219188eb0",
+    "url": "/mi-portafolio/static/media/boxicons.b0bb9677.svg"
+  },
+  {
+    "revision": "71701843aeebbb0f5dc223ae6558ab37",
+    "url": "/mi-portafolio/static/media/home.71701843.jpg"
+  }
+]);
